549c53e4c57b8cc949a65abe3a2ba13887b7f899
Nicolò <nicocriva1@gmail.com>
2021-05-28 15:14:13 +0200

    add numeri random

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//1 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
+
+//2 Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+var giocatore = Math.floor(Math.random() * 6 + 1);;
+document.getElementById('numeroGiocatore').innerHTML = ('punteggio giocatore: ') + giocatore;
+
+
+var computer = Math.floor(Math.random() * 6 + 1);;
+document.getElementById('numeroCommputer').innerHTML = ('punteggio computer: ') + computer;
+
+
+var risultato = 'Il punteggio è pari!';
+
+if (giocatore > computer){
+    var risultato = ('Il giocatore ha vinto!');
+} else if (giocatore < computer){
+    var risultato = ('Il computer ha vinto!')
+}
+document.getElementById('risultato').innerHTML = risultato;
